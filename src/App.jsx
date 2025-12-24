394d28aebdd88681133a13e67af7bffa9f8c298f
genuinelycool <genuinelycool@gmail.com>
2025-12-24 16:39:59 +0545

    9 - Working with Props in React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-const ColorfulComponent = ({ color = "blue", children }) => (
-  <div style={{ color: color }}>
-    <p>This component is {color}.</p>
-    {children}
+const ColorfulComponent = (props) => (
+  <div style={{ color: props.color ||"blue" }}>
+    <p>This component is {props.color || "blue"}.</p>
+    {props.children}
+  </div>
+);
+
+const UserProfile = (props) => (
+  <div>
+    <h1>Name: {props.name}</h1>
+    <p>Date of Birth: {props.dob}</p>
+    <p>Company: {props.company}</p>
+    <p>university: {props.university}</p>
   </div>
 );
 
 const App = () => {
   const color1 = "blue";
   const color2 = "green";
+  
+  const userDetails = {
+    name: "Mark Zuckerberg",
+    dob: "1884-05-14",
+    company: "Meta (formerly Facebook)",
+    university: "Harvard university",
+  };
   
   return (
     <div>
@@ -18,6 +34,22 @@ const App = () => {
       <ColorfulComponent color={color2}>
         <p>Green symbolizes nature, renewal, and energy.</p>
       </ColorfulComponent>
+      
+      <UserProfile 
+        // DRY (Don't Repeat Yourself)
+        {...userDetails}
+      />
+      
+      {/* 
+        
+      <UserProfile
+        name={userDetails.name}
+        dob={userDetails.dob}
+        company={userDetails.company}
+        university={userDetails.university}
+      />
+      
+      */}
     </div>
   );
 }
