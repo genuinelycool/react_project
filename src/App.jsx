56fe3dd498146f2cf46ceb2c46853e374d0763d1
genuinelycool <genuinelycool@gmail.com>
2025-12-24 16:15:25 +0545

    9 - Working with Props in React Components - with destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+const ColorfulComponent = ({ color = "blue", children }) => (
+  <div style={{ color: color }}>
+    <p>This component is {color}.</p>
+    {children}
+  </div>
+);
+
 const App = () => {
-  return <h1>React is fun!</h1>;
+  const color1 = "blue";
+  const color2 = "green";
+  
+  return (
+    <div>
+      <ColorfulComponent color={color1}>
+        <p>Blue is considered a calming color for the eyes.</p>
+      </ColorfulComponent>
+      
+      <ColorfulComponent color={color2}>
+        <p>Green symbolizes nature, renewal, and energy.</p>
+      </ColorfulComponent>
+    </div>
+  );
 }
 
 export default App;
