29d8ec9b02d5952aee0eea8cf300bfb62a11653d
genuinelycool <genuinelycool@gmail.com>
2025-12-28 19:37:11 +0545

    example: styling. 1.inline 2.external 3.module

--- src/components/StylingExample.jsx
+++ src/components/StylingExample.jsx
@@ -1,7 +1,39 @@
+import "./AppleComponent.css";
+import styles from "./AmazonComponent.module.css";
+
+const AmazonComponent = () => (
+  <div>
+    <h1>Amazon</h1>
+    <p className={styles.paragraph}>Work hard. Have fun. Make history.</p>
+  </div>
+);
+
+const AppleComponent = () => (
+  <div>
+    <h1>Apple</h1>
+    <p className="apple-paragraph">Think different.</p>
+  </div>
+);
+
+const GoogleComponent = () => {
+  const paragraphStyle = { color: "#34A853", fontSize: "1.2rem" };
+  
+  return (
+    <div>
+      <h1>Google</h1>
+      <p style={paragraphStyle}>Organizing the world$apos:s information.</p>
+    </div>
+  );
+};
+
 const StylingExample = () => {
   return (
     <>
-      
+      <hr />
+      <h2>Styling Example</h2>
+      <AmazonComponent />
+      <AppleComponent />
+      <GoogleComponent />
     </>
   );
 };
