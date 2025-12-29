4c257f15a3c3f14eced23388f75e485169ac651f
genuinelycool <genuinelycool@gmail.com>
2025-12-29 23:45:34 +0545

    example: pure vs impure components

--- src/components/PureComponentExample.jsx
+++ src/components/PureComponentExample.jsx
@@ -1,7 +1,22 @@
 const PureComponentExample = () => {
+  const Greeting = ({ name }) => <h3>Hello, {name}!</h3>
+  
+  let counter = 0;
+  const ImpureGreeting = ({ name }) => {
+    counter++;
+    return <h3>Hello, {name}! Count: {counter}</h3>;
+  };
+  
   return (
     <>
+      <hr />
+      <h2>Pure vs Impure Components</h2>
       
+      <Greeting name="John" />
+      <Greeting name="John" />
+      
+      <ImpureGreeting name="Doe" />
+      <ImpureGreeting name="Doe" />
     </>
   );
 };
