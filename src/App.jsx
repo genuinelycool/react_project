4c19ff3015a88d148af7f0d8201803a7aafd822b
genuinelycool <genuinelycool@gmail.com>
2025-12-31 06:29:25 +0545

    simple react template - 4 - Adding Interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import JSInJSXExample from "./components/JSInJSXExample";
-import StylingExample from "./components/StylingExample";
-import PropsExample from "./components/PropsExample";
-import ConditionalRendering1 from "./components/ConditionalRendering1";
-import ConditionalRendering2 from "./components/ConditionalRendering2";
-import ListRendering from "./components/ListRendering";
-import PureComponentExample from "./components/PureComponentExample";
-import PurePropsExample from "./components/PurePropsExample";
-
 const App = () => {
-  return (
-    <div>
-      <h1>Introduction & JSX - Chapter Review</h1>
-      <JSInJSXExample />
-      <StylingExample />
-      <PropsExample />
-      <ConditionalRendering1 />
-      <ConditionalRendering2 />
-      <ListRendering />
-      <PureComponentExample />
-      <PurePropsExample />
-    </div>
-  );
+  return <h1>React is fun!</h1>;
 };
 
 export default App;
