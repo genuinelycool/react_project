a69aee06ebcebb7f64790ecaea60cf0252ea32b7
genuinelycool <genuinelycool@gmail.com>
2025-12-31 06:48:46 +0545

    1 - Responding to Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 const App = () => {
-  return <h1>React is fun!</h1>;
+  const handleClick = () => console.log("Button clicked!");
+  
+  let count = 0;
+  const handleIncrement = () => {
+    count++;
+    console.log(count);
+  }
+  
+  return (
+    <>
+      {/* <button onClick={() => console.log("Button clicked!")}>*/}
+      <button onClick={handleClick}>
+        Click me
+      </button>
+      
+      <button onClick={handleIncrement}>
+        Increment
+      </button>
+      
+    </>
+  );
 };
 
 export default App;
