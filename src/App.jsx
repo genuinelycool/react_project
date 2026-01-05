5992b40b5ecd0d558be4d95d30bbd27074af1b81
genuinelycool <genuinelycool@gmail.com>
2026-01-05 09:56:23 +0545

    10 - Trigger=render=VirtualDOM=Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
 import ShoppingCart from "./ShoppingCart";
 
-const App = () => <ShoppingCart />
+const App = () => {
+  const [showCart, setShowCart] = useState(true);
+  
+  // Function to remove the ShoppingCart
+  const removeCart = () => {
+    setShowCart(false); // This will hide the ShoppingCart
+  };
+  
+  return (
+    <div>
+      {/* Button to remove the ShoppingCart*/}
+      <button onClick={removeCart}>Remove ShoppingCart</button>
+      
+      {/* Conditionally render ShoppingCart*/}
+      {showCart && <ShoppingCart />}
+    </div>
+  );
+};
 
 export default App;
